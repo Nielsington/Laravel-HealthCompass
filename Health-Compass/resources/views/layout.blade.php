e54502a686ccb1a8a99a8f72193e516d9a63eac2
Nielsington <declercqniels@hotmail.com>
2023-04-13 21:41:40 +0200

    Add layout view

---
+++ Health-Compass/resources/views/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital@1&display=swap" rel="stylesheet">
+    @yield('title')
+    @vite('resources/css/app.css')
+</head>
+<body>
+    @include('./includes/header')
+    <div>
+        <x-navigation />
+    </div>
+    @yield('content')
+    @include('./includes/footer')
+</body>
+</html>
