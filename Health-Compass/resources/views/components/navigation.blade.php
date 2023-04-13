0b14bb1ce38ca39f9682473774faa82479bba6a0
Nielsington <declercqniels@hotmail.com>
2023-04-13 21:42:58 +0200

    Add navigation component

---
+++ Health-Compass/resources/views/components/navigation.blade.php
@@ -0,0 +1,10 @@
+<nav>
+    <ul>
+        <a href="#">Home</a>
+        <a href="#">Add day</a>
+        <a href="#">About page</a>
+        <a href="#">Contact page</a>
+    </ul>
+</nav>
+
+{{--  <a href="{{route('#')}}">Contact page</a> --}}
