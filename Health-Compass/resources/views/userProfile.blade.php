54d3c23f9fc44b39c9fe1fa4bfd80d87ee2281ff
Nielsington <declercqniels@hotmail.com>
2023-04-13 21:41:20 +0200

    Add user profile view

---
+++ Health-Compass/resources/views/userProfile.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('title')
+    <title> Profile </title>
+@endsection
+
+@section('content')
+<main class='containers-wrap'>
+    <div class='nutrition'>FOOD/DRINK </div>
+    <div class='sleep'>MOOD/SLEEP </div>
+    <div class='activity'>ACTIVITY </div>
+    <div class='bodyPic'>BODY PIC </div>
+</main>
+@endsection
