f381cc080f99e494876078977d34cfb94862ca5d
Nielsington <declercqniels@hotmail.com>
2023-04-14 18:56:06 +0200

    Add navigation component

--- Health-Compass/resources/views/components/navigation.blade.php
+++ Health-Compass/resources/views/components/navigation.blade.php
@@ -1,10 +1,9 @@
 <nav>
     <ul>
-        <a href="#">Home</a>
-        <a href="#">Add day</a>
-        <a href="#">About page</a>
-        <a href="#">Contact page</a>
+        <a href="{{route('home')}}">Home</a>
+        <a href="{{route('newDay')}}">New day</a>
+        <a href="{{route('userProfile')}}">Profile</a>
+        <a href="{{route('contact')}}">Contact</a>
+        <a href="{{route('about')}}">About</a>
     </ul>
 </nav>
-
-{{--  <a href="{{route('#')}}">Contact page</a> --}}
