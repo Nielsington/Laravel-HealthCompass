f61ccb12c0db36e7af0e0a11aa37ecf725b965e7
Nielsington <declercqniels@hotmail.com>
2023-04-14 18:53:00 +0200

    Add routes

--- Health-Compass/routes/web.php
+++ Health-Compass/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomepageController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewDayController;
+use App\Http\Controllers\UserProfileController;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('userProfile');
-});
+Route::get('/', [HomepageController::class, 'show'])->name('home');
+Route::get('/new-day', [NewDayController::class, 'show'])->name('newDay');
+Route::get('/profile', [UserProfileController::class, 'show'])->name('userProfile');
+Route::get('/contact', [ContactController::class, 'show'])->name('contact');
+Route::get('/about', [AboutController::class, 'show'])->name('about');
