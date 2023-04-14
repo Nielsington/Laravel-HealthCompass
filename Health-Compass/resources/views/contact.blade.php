62ccf5266a1f4a53e0f3fb50cf67d75155704b98
Nielsington <declercqniels@hotmail.com>
2023-04-14 18:54:03 +0200

    Add contact view

---
+++ Health-Compass/resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('title')
+    <title> Contact </title>
+@endsection
+
+@section('content')
+    <main>
+        <div class='container-style'>
+            
+        </div>
+    </main>
+@endsection
