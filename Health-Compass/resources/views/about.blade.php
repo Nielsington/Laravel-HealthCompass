125eea582b68848b81f2905c12a15b1513ba4bef
Nielsington <declercqniels@hotmail.com>
2023-04-14 18:56:21 +0200

    Add about view

---
+++ Health-Compass/resources/views/about.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+
+@section('title')
+    <title> About </title>
+@endsection
+
+@section('content')
+    <main>
+        <div class='container-style about-container'>
+            <p>
+                <span id='big-letter'>W</span>elcome to The Health Compass!<br><br>
+                Our website is dedicated to helping you reach your health and fitness goals in a personalized and comprehensive way. By allowing you to track your meals, sports activity, mood, and sleep, we help you develop a holistic understanding of your body and make informed decisions about your lifestyle. <br><br>
+                We believe that health is not just about the food we eat or the exercise we do, but also about how we feel and how we rest. That's why we offer a range of tools to help you monitor your overall wellbeing. With our easy-to-use interface, you can log the meals you eat and track your nutritional intake of fat, protein, and carbohydrates. You can also keep a record of your sports activity, including the duration and intensity of your workouts, to ensure you're meeting your fitness goals.<br><br>
+                In addition to tracking your physical activity, we understand that mental wellbeing is just as important. That's why we provide a feature for you to track your mood and sleep duration, helping you understand how these factors affect your overall health. You can also set a weight goal for yourself and track your progress over time by uploading a picture of your body, allowing you to see the changes as they happen.<br><br>
+                At The Health Compass, we believe in the power of personalization. By giving you the tools to track your own health, you can create a unique plan that works for you and your lifestyle. We hope to empower you to take control of your health and achieve your goals, whatever they may be.
+            </p>
+        </div>
+    </main>
+@endsection
