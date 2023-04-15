6a8a5970bdfc1c064708722e4458bfa0a0be3aea
Nielsington <declercqniels@hotmail.com>
2023-04-15 21:43:44 +0200

    Remove unnecessary links in navigation

--- Health-Compass/resources/views/components/navigation.blade.php
+++ Health-Compass/resources/views/components/navigation.blade.php
@@ -1,8 +1,6 @@
 <nav>
     <ul>
-        <a href="{{route('home')}}">Home</a>
-        <a href="{{route('newDay')}}">New day</a>
-        <a href="{{route('userProfile')}}">Profile</a>
+        <a href="{{route('dashboard')}}">Dashboard</a>
         <a href="{{route('contact')}}">Contact</a>
         <a href="{{route('about')}}">About</a>
     </ul>
