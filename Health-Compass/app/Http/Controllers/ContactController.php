35ee99de2b480e275fa950a13363d030bd152adf
Nielsington <declercqniels@hotmail.com>
2023-04-15 21:52:10 +0200

    Refactor according to PSR-12

--- Health-Compass/app/Http/Controllers/ContactController.php
+++ Health-Compass/app/Http/Controllers/ContactController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    public function show() {
+    public function show() 
+    {
         return view('contact');
     }
 }
