1406c97d047bacb00fed2d9586507b15504344c2
Nielsington <declercqniels@hotmail.com>
2023-04-15 21:45:28 +0200

    Add migration for activity_log table

---
+++ Health-Compass/database/migrations/2023_04_15_171046_create_activity_log_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activity_log', function (Blueprint $table) {
+            $table->id();
+            $table->string('Activity_type', 255);
+            $table->integer('Activity_minutes');
+            $table->integer('Kcal_burned');
+            $table->text('Notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activity_log');
+    }
+};
