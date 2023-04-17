6eafbaf9924878652618840ac4cc04f1d8bafa4c
Nielsington <declercqniels@hotmail.com>
2023-04-18 01:00:13 +0200

    Change form labels

--- Health-Compass/resources/views/dashboard.blade.php
+++ Health-Compass/resources/views/dashboard.blade.php
@@ -14,6 +14,29 @@
     </div> --}}
 
     <div class='containers-wrap'>
+        <div id='activityContainer'>
+            @if(isset($activityData))
+                <p>Form has been submitted</p>
+            @else
+                <form class="dashboard-forms" action="/submit-activity" method="POST">
+                    @csrf
+                    <label for="activity-type">Sport</label>
+                    <input type="text" name="activity-type" id="activity-type">
+                    <label for="activity-duration">Duration (minutes)</label>
+                    <input type="text" name="activity-duration" id="activity-duration">
+                    <label for="kcal">Calories burned</label>
+                    <input type="text" name="kcal" id="kcal">
+                    <label for="notes">Notes</label>
+                    <textarea name="dream" id="dream" cols="30" rows="10" placeholder="Any additional notes?"></textarea>
+                    <button type="submit" id="submit-activity">SAVE</button>
+                </form>
+            @endif
+        </div>
+
+        <div id='bodyPicContainer'>
+            bodyPic
+        </div>
+
         <div id='sleepContainer'>
             @if(isset($sleepData))
                 <p>Form has been submitted</p>
@@ -22,36 +45,14 @@
                     @csrf
                     <label for="mood">Mood</label>
                     <input type="text" name="mood" id="mood">
-                    <label for="sleepHours">Hours of sleep</label>
+                    <label for="sleepHours">Sleep duration</label>
                     <input type="text" name="sleepHours" id="sleepHours">
-                    <label for="dream">If you had a dream, describe it here</label>
-                    <textarea name="dream" id="dream" cols="30" rows="10"></textarea>
+                    <label for="dream">Dream</label>
+                    <textarea name="dream" id="dream" cols="30" rows="10" placeholder="If you had a dream, describe it here"></textarea>
                     <button type="submit" id="submit-sleep">SAVE</button>
                 </form>
             @endif
             
-        </div>
-
-        <div id='activityContainer'>
-            @if(isset($activityData))
-                <p>Form has been submitted</p>
-            @else
-                <form class="dashboard-forms" action="/submit-activity" method="POST">
-                    @csrf
-                    <label for="activity-type">What sport did you do?</label>
-                    <input type="text" name="activity-type" id="activity-type">
-                    <label for="activity-duration">For how long? (minutes)</label>
-                    <input type="text" name="activity-duration" id="activity-duration">
-                    <label for="kcal">How many calories did you burn?</label>
-                    <input type="text" name="kcal" id="kcal">
-                    <label for="notes">Any additional notes?</label>
-                    <textarea name="dream" id="dream" cols="30" rows="10"></textarea>
-                    <button type="submit" id="submit-activity">SAVE</button>
-                </form>
-            @endif
-        </div>
-        <div id='bodyPicContainer'>
-            bodyPic
         </div>
         <div id='newDay'>
             <p>NEW DAY</p>
