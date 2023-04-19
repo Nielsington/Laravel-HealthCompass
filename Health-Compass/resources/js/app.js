ed596faa41af97866a9a220f5c1f3fbfecdfd85a
Nielsington <declercqniels@hotmail.com>
2023-04-19 17:08:42 +0200

    Add mood form

--- Health-Compass/resources/js/app.js
+++ Health-Compass/resources/js/app.js
@@ -1,1 +1,37 @@
 import './bootstrap';
+
+const addButton = document.getElementById('addMood');
+
+
+const moodFormSwitch = () => {
+
+    const moodContainer = document.querySelector('#mood-container');
+
+    addButton.style.display = 'none';
+
+    const form = document.createElement("form");
+    form.setAttribute("id", 'moodForm');
+
+    const label = document.createElement("label");
+    label.innerHTML = "How you feelin' buddy?";
+    label.setAttribute("for", "moodInput")
+
+    const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("name", "moodInput");
+    input.setAttribute("id", "moodInput");
+
+    const submitBtn = document.createElement("button");
+    submitBtn.setAttribute("type", "submit");
+    submitBtn.setAttribute("id", "saveMood");
+    submitBtn.textContent = "Save";
+
+    moodContainer.appendChild(form);
+    form.appendChild(label);
+    form.appendChild(input);
+    form.appendChild(submitBtn);
+}
+
+addButton.addEventListener('click', ()=>{
+    moodFormSwitch();
+})
