3456e284da259fc2225f3879677e342b38ddb1bc
Nielsington <declercqniels@hotmail.com>
2023-04-20 19:44:15 +0200

    Add loop for activity data
    - Add 'add activity' container
    - Add weather widget
    - Add daily steps container
    - Add max calories container
    - Add mood tracker container
    - Add dream catcher container
    - Add images to add data to containers

--- Health-Compass/resources/views/dashboard.blade.php
+++ Health-Compass/resources/views/dashboard.blade.php
@@ -14,26 +14,37 @@
     </div> --}}
 <div id="flex-wrap">
     <aside id='activity-container'>
+        <div id='addActivity-container'>
+            <p id='activity-container-title'>Today's activities</p>
+            <a id='addActivity' href="{{route('activityForm')}}">
+                <img id="addActivityIcon" src="{{asset('./images/plus.png')}}" alt="Add sign icon">
+            </a>
+        </div>
         @if(isset($activityData))
-            {{-- <p>Form has been submitted</p> --}}
-            <div id='activity_stats'>
-                <div id="activity">
-                    <img src="{{asset('./images/sport.png')}}" alt="sport icon">
-                    <p>{{$activityData->Activity_type}}</p>
-                </div>
-                <div id="activityDuration">
-                    <img src="{{asset('./images/stopwatch.png')}}" alt="stopwatch icon">
-                    <p>{{$activityData->Activity_minutes}} minutes</p>
-                </div>
-                <div id="kcalBurned">
-                    <img src="{{asset('./images/kcal.png')}}" alt="kcal icon">
-                    <p>{{$activityData->Kcal_burned}} calories burned!</p>
-                </div>
-            </div>
-            {{-- TODO:if notes = NULL then <p>No notes to report</p> --}}
-            {{-- TODO: show this in detail page --}}
-            {{-- <img id="notes" src="{{asset('./images/notes.png')}}" alt="notes icon">
-            <p id='activityReport'>{{$activityData->Notes}}</p> --}}
+            @foreach($activityData as $activity)
+                @if($loop->last)
+                    <div id='last_activity_stats'>
+                @else
+                    <div id='activity_stats'>
+                @endif
+                        <div id="activity">
+                            <img src="{{asset('./images/sport.png')}}" alt="sport icon">
+                            <p>{{$activity->Activity_type}}</p>
+                        </div>
+                        <div id="activityDuration">
+                            <img src="{{asset('./images/stopwatch.png')}}" alt="stopwatch icon">
+                            <p>{{$activity->Activity_minutes}} minutes</p>
+                        </div>
+                        <div id="kcalBurned">
+                            <img src="{{asset('./images/kcal.png')}}" alt="kcal icon">
+                            <p>{{$activity->Kcal_burned}} calories</p>
+                        </div>
+                        {{-- TODO: detail page --}}
+                        <form action="/activity-details" method="get">
+                            <button>See details</button>
+                        </form>
+                    </div>
+            @endforeach
         @else
             {{-- TODO: Add plus sign logo to go to activityForm view --}}
             <a href="{{route('activityForm')}}"><img id="addActivity" src="{{asset('./images/plus.png')}}" alt="Add sign icon"></a>
@@ -42,22 +53,28 @@
     <main id='grid-container'>
 
         <section id='weather-widget'>
-            weather/time here
+            <div id="ww_9c42bdaca2406" v='1.3' loc='auto' a='{"t":"horizontal","lang":"en","sl_lpl":1,"ids":[],"font":"Arial","sl_ics":"one_a","sl_sot":"celsius","cl_bkg":"image","cl_font":"#FFFFFF","cl_cloud":"#FFFFFF","cl_persp":"#81D4FA","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722","el_whr":3,"el_phw":3}'>
+                Weather Data Source: 
+                <a href="https://wetterlang.de/wetter_21_tage/" id="ww_9c42bdaca2406_u" target="_blank">Wettervorhersage 21 tage</a>
+            </div>
         </section>
 
         <section id='stepCount'>
-            steps here
+            <p>Daily steps</p>
+            <p>/10.000</p>
+            <form action="/submit-steps" method="post">
+                <input type="text" name="stepsInput" placeholder="Enter steps">
+                <button type="submit">+</button>
+            </form>
         </section>
 
-        
         <section id='kcalCount'>
-            max kcal
+            <p>Max kcal/day</p>
+            <p># / #</p>
         </section>
 
-
-
-        {{-- TODO: style the form you generate with javascript --}}
         <div id="mood-container">
+            <p>Mood tracker</p>
             @if(isset($moodData))
                 <img src="{{asset('./images/mood.png')}}" alt="mood icon">
                 <p>{{$sleepData->mood}} mood</p>
@@ -67,6 +84,7 @@
         </div>
 
         <div id='sleep-container'>
+            <p>Dream Catcher</p>
             @if(isset($sleepData))
                     <div id="sleepDuration">
                         <img src="{{asset('./images/sleepDuration.png')}}" alt="sleep duration icon">
@@ -76,16 +94,7 @@
                 <img id="dreamCatcherIcon" src="{{asset('./images/dream.png')}}" alt="dream catcher icon">
                 <p id='dreamReport'>{{$sleepData->dream_description}}</p>
             @else
-                {{-- <form id="moodSleepForm" action="/submit-sleep" method="POST">
-                    @csrf
-                    <label for="mood">Mood</label>
-                    <input type="text" name="mood" id="mood">
-                    <label for="sleepHours">Sleep duration</label>
-                    <input type="text" name="sleepHours" id="sleepHours">
-                    <label for="dream">Dream</label>
-                    <textarea name="dream" id="dream" cols="30" rows="10" placeholder="If you had a dream, describe it here"></textarea>
-                    <button type="submit" id="submit-sleep">SAVE</button>
-                </form> --}}
+                <a href="/create-sleep"><img src="./images/plus.png" alt="Add sign icon" id="addSleep"></a>   
             @endif
         </div>
 
