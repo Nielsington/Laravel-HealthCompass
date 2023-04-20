f9466473c083bbfc58356083a9f7ae2c478f80f7
Nielsington <declercqniels@hotmail.com>
2023-04-20 19:45:08 +0200

    Add empty detail page view

---
+++ Health-Compass/resources/views/activityDetail.blade.php
@@ -0,0 +1,4 @@
+{{-- TODO:if notes = NULL then <p>No notes to report</p> --}}
+{{-- TODO: show this in detail page --}}
+{{-- <img id="notes" src="{{asset('./images/notes.png')}}" alt="notes icon">
+<p id='activityReport'>{{$activityData->Notes}}</p> --}}
