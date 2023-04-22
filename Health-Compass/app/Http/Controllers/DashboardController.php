929adaed4430ac1865214c296b987fd4e3f96afd
Nielsington <declercqniels@hotmail.com>
2023-04-22 22:19:25 +0200

    Add type hinting for functions
    - Add handle and fetch for steps form
    - Add handle and fetch for mood form

--- Health-Compass/app/Http/Controllers/DashboardController.php
+++ Health-Compass/app/Http/Controllers/DashboardController.php
@@ -5,7 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\SleepLog;
 use App\Models\ActivityLog;
+use App\Models\MoodLog;
+use App\Models\StepsLog;
 use Carbon\Carbon;
+use Illuminate\Routing\Redirector;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Collection;
+use Ramsey\Uuid\Type\Integer;
 
 class DashboardController extends Controller
 {
@@ -13,8 +19,14 @@ class DashboardController extends Controller
     {
         $sleepData = $this->fetchSleepData();
         $activityData = $this->fetchActivityData();
+        $moodData = $this->fetchMoodData();
+        $stepsData = $this->fetchStepsData();
 
-        return view('dashboard', ['sleepData'=>$sleepData, 'activityData'=>$activityData]);
+        return view('dashboard', ['sleepData'=>$sleepData, 
+                                               'activityData'=>$activityData, 
+                                               'moodData'=>$moodData,
+                                               'stepsData'=>$stepsData
+                                            ]);
     }
 
     //Activity Form
@@ -24,7 +36,7 @@ class DashboardController extends Controller
         return view('activityForm');
     }
 
-    public function handleActivityForm(Request $request)
+    public function handleActivityForm(Request $request): RedirectResponse
     {
         $activityData = new ActivityLog();
 
@@ -37,10 +49,48 @@ class DashboardController extends Controller
         return redirect()->route('dashboard');
     }
 
-    private function fetchActivityData()
+    private function fetchActivityData(): Collection
     {
         $activityData = ActivityLog::whereDate('created_at', Carbon::today())->get();
         return $activityData;
+    }
+
+    //Steps Form
+
+    public function handleStepsForm(Request $request): RedirectResponse
+    {
+        $steps = new StepsLog();
+        $steps->steps = $request['stepsInput'];
+        $steps->save();
+
+        return redirect('/');
+    }
+
+    private function fetchStepsData(): int
+    {
+        $stepsData = StepsLog::whereDate('created_at', Carbon::today())->get();
+        $stepTotal = 0;
+        foreach ($stepsData as $steps) {
+            $stepTotal += $steps->steps;
+        }
+        return $stepTotal;
+    }
+
+    //Mood Form
+
+    public function handleMoodForm(Request $request): Redirector
+    {
+        $mood = new MoodLog();
+        $mood->mood = $request['mood'];
+        $mood->save();
+
+        return redirect('/');
+    }
+
+    private function fetchMoodData(): MoodLog
+    {
+        $moodData = MoodLog::whereDate('created_at', Carbon::today())->first();
+        return $moodData;
     }
 
     //Sleep Form
@@ -50,7 +100,7 @@ class DashboardController extends Controller
         return view('sleepForm');
     }
 
-    public function handleSleepForm(Request $request) 
+    public function handleSleepForm(Request $request): RedirectResponse 
     {
         $sleepData = new SleepLog();
 
@@ -61,7 +111,7 @@ class DashboardController extends Controller
         return redirect()->route('dashboard');
     }
 
-    private function fetchSleepData()
+    private function fetchSleepData(): SleepLog
     {
         $sleepData = SleepLog::whereDate('created_at', Carbon::today())->first();
         return $sleepData;
