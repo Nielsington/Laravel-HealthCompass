e19987b64e76d7f8cb89ee716040272264b6074c
Nielsington <declercqniels@hotmail.com>
2023-04-22 05:39:28 +0200

    Add li to nav

--- Health-Compass/resources/views/components/navigation.blade.php
+++ Health-Compass/resources/views/components/navigation.blade.php
@@ -1,6 +1,7 @@
 <nav>
     <ul>
         <li><a href="{{route('dashboard')}}">Dashboard</a></li>
+        <li><a href="{{route('all-activities')}}">All activities</a></li>
         <li><a href="{{route('contact')}}">Contact</a></li>
         <li><a href="{{route('about')}}">About</a></li>
     </ul>
