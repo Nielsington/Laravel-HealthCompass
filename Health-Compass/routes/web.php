7f9ba0d685f2fbfc95d03bedeacb8d775d3c2b13
Nielsington <declercqniels@hotmail.com>
2023-04-22 05:37:52 +0200

    Add route for 'all activities' view

--- Health-Compass/routes/web.php
+++ Health-Compass/routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\AboutController;
 use App\Http\Controllers\NewDayController;
 use App\Http\Controllers\UserProfileController;
 use App\Http\Controllers\ContactController;
+use App\Models\ActivityLog;
+use Illuminate\Support\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,19 +21,22 @@ use App\Http\Controllers\ContactController;
 */
 
 Route::get('/', [DashboardController::class, 'show'])->name('dashboard');
+Route::get('/contact', [ContactController::class, 'show'])->name('contact');
+Route::get('/about', [AboutController::class, 'show'])->name('about');
 
 Route::get('/create-activity', [DashboardController::class, 'showActivityForm'])->name('activityForm');
 Route::post('/submit-activity', [DashboardController::class, 'handleActivityForm']);
 
 Route::get('/create-sleep', [DashboardController::class, 'showSleepForm']);
-
-
 Route::post('/submit-sleep', [DashboardController::class, 'handleSleepForm']);
 
+Route::get('/{ActivityLog:Activity_type', function(ActivityLog $Activity_type)
+    {
+        $Activities = $Activity_type->whereDate('created_at', '=', Carbon::today())->get();
+        return view('allActivities', ['Activities' => $Activities]);
+    })->name('all-activities');
 
-Route::get('/contact', [ContactController::class, 'show'])->name('contact');
-Route::get('/about', [AboutController::class, 'show'])->name('about');
 
 // TODO: Make route for /submit-mood
 // TODO: Make route for /submit-steps
-//
+//TODO: detail page ->name('detail');
